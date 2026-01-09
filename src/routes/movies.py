e2d8cdec3d30b82ba63b859bd25e43abfef2cc31
Oleksiy <kotov.oleskiy@icloud.com>
2026-01-09 21:28:48 +0200

    Fix path parameter for endpoint movie_id. Fix HTTPException for a page number

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -23,7 +23,10 @@ async def get_movies(page: int = Query(1, ge=1, description="Page number (starti
     total_pages = (total_items + per_page - 1) // per_page
 
     if page > total_pages:
-        raise HTTPException(...)
+        raise HTTPException(
+            status_code=404,
+            detail=f"Page {page} not found. Total pages: {total_pages}"
+        )
 
     offset = (page - 1) * per_page
     query = select(MovieModel).order_by(MovieModel.id).offset(offset).limit(per_page)
@@ -44,13 +47,13 @@ async def get_movies(page: int = Query(1, ge=1, description="Page number (starti
     )
 
 
-@router.get("/{id}", response_model=MovieDetailResponseSchema)
+@router.get("/{movie_id}/", response_model=MovieDetailResponseSchema)
 async def get_movie(id: int, db: AsyncSession = Depends(get_db)):
     query = select(MovieModel).where(MovieModel.id == id)
     result = await db.execute(query)
     movie = result.scalar_one_or_none()
 
-    if not movie:
+    if movie is None:
         raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
 
     return movie
